89ebda9aae5f0d928b3b08c124977ef96224e4cd
muhadi13 <58276684+muhadi13@users.noreply.github.com>
2019-11-28 08:01:40 +0700

    Add files via upload

---
+++ twLSN.js
@@ -0,0 +1,8 @@
+var twLSN = [
+'basyarahtour.blogspot.com','www.basyarahtour.blogspot.com','basyarahtour.com','www.basyarahtour.com',
+'www.domainpembeli-2.com',
+'www.domainpembeli-3.com',
+'www.domainpembeli-4.com',
+'www.domainpembeli-dst.com',
+];
+var redirectURL = 'https://bit.ly/37KxPgL';

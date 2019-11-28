91ffb45c239980c4bb8aac33821982c87f5ea307
muhadi13 <58276684+muhadi13@users.noreply.github.com>
2019-11-28 08:32:02 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,5 +1,5 @@
 var twLSN = [
-'basyarahtour.blogspot.com','www.basyarahtour.blogspot.com','basyarahtour.com','www.basyarahtour.com',
+
 'handycraftbelitung.blogspot.com','www.handycraftbelitung.blogspot.com','handycraftbelitung.com','www.handycraftbelitung.com',
 'www.domainpembeli-3.com',
 'www.domainpembeli-4.com',

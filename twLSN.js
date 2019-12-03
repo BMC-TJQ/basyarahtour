c8448d62c9c7c9eb86b4be837f2842b687058b31
Blackfish <58276684+BMC-TJQ@users.noreply.github.com>
2019-12-03 17:45:56 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,8 +1,4 @@
 var twLSN = [
 'basyarahtour.blogspot.com','www.basyarahtour.blogspot.com','basyarahtour.com','www.basyarahtour.com',
-'handycraftbelitung.blogspot.com','www.handycraftbelitung.blogspot.com','handycraftbelitung.com','www.handycraftbelitung.com',
-'www.domainpembeli-3.com',
-'www.domainpembeli-4.com',
-'www.domainpembeli-dst.com',
 ];
 var redirectURL = 'https://bit.ly/37KxPgL';
